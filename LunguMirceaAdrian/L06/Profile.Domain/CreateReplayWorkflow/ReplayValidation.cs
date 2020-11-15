a472242c03bf8569acb972837d3ea71b1d8b5dd1
LunguMircea <61224289+LunguMircea@users.noreply.github.com>
2020-11-15 12:51:14 +0200

    Tema6

---
+++ LunguMirceaAdrian/L06/Profile.Domain/CreateReplayWorkflow/ReplayValidation.cs
@@ -0,0 +1,102 @@
+﻿using CSharp.Choices;
+using LanguageExt.Common;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+
+namespace Question.Domain.CreateReplayWorkflow
+{
+    [AsChoice]
+    public static partial class ValidationReplayResult
+    {
+        public interface IReplayValidationResult { }
+
+        public class ReplayUnchecked : IReplayValidationResult
+        {
+            public CreateReplayCMD Replay { get; private set; }
+            public ReplayUnchecked(CreateReplayCMD arg)
+            {
+                Replay = arg;
+            }
+        }
+
+        public class InvalidReplay : IReplayValidationResult
+        {
+            public readonly List<string> Errors;
+            public InvalidReplay(List<string> errors)
+            {
+                Errors = new List<string>(errors);
+            }
+        }
+
+
+        public class ValidReplay : IReplayValidationResult
+        {
+            public Guid ReplayID { get; private set; }
+            public ReplayUnchecked Replay { get; private set; }
+            public ValidReplay(Guid replayID, ReplayUnchecked replay)
+            {
+                Replay = replay;
+                ReplayID = replayID;
+            }
+        }
+
+        public class ReplayProfanityCheckedFailed : IReplayValidationResult
+        {
+            public Guid ProfanityCheckID { get; private set; }
+            public ValidReplay Replay { get; private set; }
+
+            public String Msg;
+            public ReplayProfanityCheckedFailed(Guid pcID, ValidReplay replay,String msg)
+            {
+                ProfanityCheckID = pcID;
+                Replay = replay;
+                Msg = msg;
+            }
+
+        }
+
+
+        public class ReplayProfanityCheckedPassed : IReplayValidationResult
+        {
+            public Guid ProfanityCheckID{ get; private set; }
+            public ValidReplay Replay { get; private set; }
+            public ReplayProfanityCheckedPassed(Guid pcID, ValidReplay replay)
+            {
+                ProfanityCheckID = pcID;
+                Replay = replay;
+            }
+            
+         }
+
+        public class ReplayAcknoledgmentNotSent : IReplayValidationResult
+        {
+            public ReplayProfanityCheckedPassed Replay { get; private set; }
+            public ReplayAcknoledgmentNotSent(ReplayProfanityCheckedPassed replay)
+            {
+                Replay = replay;
+            }
+        }
+
+            public class ReplayAcknoledgmentSent : IReplayValidationResult
+        {
+            public ReplayAcknoledgmentNotSent Replay { get; private set; }
+            public Guid QuestionAuthEMailID { get; private set; }
+            public Guid ReplayAuthEMailID { get; private set; }
+            public ReplayAcknoledgmentSent(ReplayAcknoledgmentNotSent replay, Guid questionAuthEMailID,Guid replayAuthEMailID)
+            {
+                Replay = replay;
+                QuestionAuthEMailID = questionAuthEMailID;
+                ReplayAuthEMailID = replayAuthEMailID;
+                SendEmails();
+            }
+            private void SendEmails() {}
+        }
+        public class ReplayMatchError : IReplayValidationResult{ }
+
+
+    }
+
+}
---
+++ LunguMirceaAdrian/L06/Test.App/Program.cs
@@ -0,0 +1,168 @@
+﻿using Question.Domain.CreateReplayWorkflow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using static Question.Domain.CreateReplayWorkflow.ValidationReplayResult;
+
+namespace Test.App
+{
+    class Program
+    {
+        private static bool finished;
+        static void Main(string[] args)
+        {
+            finished = false;
+            CreateReplayCMD unvalidatedReplay = new CreateReplayCMD(1, "1+1=2", "LunguMirceaAdrian","exemplu@gmail.com", "exemplu@gmail.com");
+            var validateReplayCMDResult = ProcessReplay(unvalidatedReplay);
+            do   // Its cute this works
+            {
+                validateReplayCMDResult.Match(
+                    ProcessReplayUnckecked,
+                    ProcessReplayInvalid,
+                    ProcessReplayValid,
+                    ProcessReplayProfanityCheckFailed,
+                    ProcessReplayProfanityCheckPassed,
+                    ProcessReplayAcknoledgementNotSent,
+                    ProcessReplayAcknoledgmentSent,
+                    ProcessReplayError
+                    );
+                Console.WriteLine("\n-----------------------------------\n");
+                validateReplayCMDResult = ProcessReplay(validateReplayCMDResult);
+            } while ( !finished );
+            Console.ReadLine();
+        }
+
+        private static IReplayValidationResult ProcessReplayError(ReplayMatchError arg)
+        {
+            Console.WriteLine("Something went wrong");
+            return null;
+        }
+
+        private static IReplayValidationResult ProcessReplayUnckecked(ReplayUnchecked arg)
+        {
+            Console.WriteLine("This replay is unchecked and has no ID");
+            return arg;
+        }
+
+        private static IReplayValidationResult ProcessReplayAcknoledgmentSent(ReplayAcknoledgmentSent arg)
+        {
+            Console.WriteLine("Replay with ID: " + arg.Replay.Replay.Replay.ReplayID + "\nHas sent the acknoledgement");
+            Console.WriteLine("Has sent mail to the replay author, mail ID is: " + arg.ReplayAuthEMailID);
+            Console.WriteLine("Has sent mail to the question author, mail ID is: " + arg.QuestionAuthEMailID);
+            Console.WriteLine("-----Works done !!!-----");
+            return arg;
+        }
+
+        private static IReplayValidationResult ProcessReplayAcknoledgementNotSent(ReplayAcknoledgmentNotSent arg)
+        {
+            Console.WriteLine("Replay with ID: " + arg.Replay.Replay.ReplayID + "\nHas yet to sent the acknoledgement");
+            return arg;
+        }
+
+        private static IReplayValidationResult ProcessReplayProfanityCheckPassed(ReplayProfanityCheckedPassed arg)
+        {
+            Console.WriteLine("Replay with ID: " + arg.Replay.ReplayID + "\nPassed profanity check " + arg.ProfanityCheckID);
+            return arg;
+        }
+
+        private static IReplayValidationResult ProcessReplayProfanityCheckFailed(ReplayProfanityCheckedFailed arg)
+        {
+            Console.WriteLine("Replay with ID: " + arg.Replay.ReplayID + "\nFailed profanity check " + arg.ProfanityCheckID);
+            return arg;
+        }
+
+        private static IReplayValidationResult ProcessReplayValid(ValidReplay arg)
+        {
+            Console.WriteLine("Replay with ID: " + arg.ReplayID + "\nHas been validated");
+            return arg;
+        }
+
+        private static IReplayValidationResult ProcessReplayInvalid(InvalidReplay arg)
+        {
+            Console.WriteLine("Replay is invalid:");
+            foreach (var error in arg.Errors)
+                Console.WriteLine(error);
+            return arg;
+        }
+
+        public static IReplayValidationResult ProcessReplay(object obj)
+        {
+            switch (obj.GetType().Name)
+            {
+                
+                case "CreateReplayCMD":
+                    return new ReplayUnchecked((CreateReplayCMD)obj);
+                    break;
+
+                case "ReplayUnchecked":                     // We do validity check
+                    var validationErrors = new List<string>();
+                    
+                    ReplayUnchecked aux = (ReplayUnchecked)obj;
+                 
+          
+                    if (new Random().Next(3) > 1) 
+                    {                       
+                        
+                         validationErrors.Add("Problems with replay text length" ); // ToDo
+                    }
+                    if (new Random().Next(3) > 1)
+                    {
+                        validationErrors.Add("Problems with email address");
+                    }
+                    if (validationErrors.Length() != 0)
+                    {
+                        return new InvalidReplay(validationErrors);
+                    }
+                    return new ValidReplay(Guid.NewGuid(), (ReplayUnchecked)obj);
+                    break;
+
+
+                case "InvalidReplay":              // Nothing we can do with it end
+                    finished = true;
+                    return (InvalidReplay)obj;
+                    break;
+
+                case "ValidReplay":                 // We do profanity check
+                    if (new Random().Next(3) > 1)
+                    {
+                        return new ReplayProfanityCheckedFailed(Guid.NewGuid(), (ValidReplay)obj, "Profanity Check failed");
+                    }
+                    return new ReplayProfanityCheckedPassed(Guid.NewGuid(), (ValidReplay)obj);
+
+                    break;
+
+                case "ReplayProfanityCheckedFailed":   // Nothing more we can do with it end
+                    finished = true;
+                    return (ReplayProfanityCheckedFailed)obj;
+                    break;
+
+                case "ReplayProfanityCheckedPassed":                 // Now we can send the mails
+                    return new ReplayAcknoledgmentNotSent( (ReplayProfanityCheckedPassed)obj);
+                  
+                    break;
+
+                case "ReplayAcknoledgmentNotSent":                  // Try resend
+                    if (new Random().Next(3) > 10)
+                    {
+                      //  Console.WriteLine("Haven't sent the mails yet please be pacient");
+                        return (ReplayAcknoledgmentNotSent)obj;
+                    }
+                    //Console.WriteLine("Have finished sending the mails :D");
+                    return new ReplayAcknoledgmentSent((ReplayAcknoledgmentNotSent)obj, Guid.NewGuid(), Guid.NewGuid());
+                    break;
+
+                case "ReplayAcknoledgmentSent":   // Jobs done the END
+                    finished = true;
+                    return (ReplayAcknoledgmentSent)obj;
+                    break;
+
+
+                default: return new ReplayMatchError();   // Somthing went really wrong
+           
+
+            }
+
+        }
+    }
+}
